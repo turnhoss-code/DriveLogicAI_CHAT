aec1afc231b21b88d43a6927713f09232ebf83ae
Don T <turnhoss187@gmail.com>
2026-08-21 13:20:06 -0400

    chore: add google client ID to capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.DriveLogicAI_chat',
   appName: 'DriveLogic AI',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    GoogleAuth: {
+      scopes: ["profile", "email", "https://www.googleapis.com/auth/drive"],
+      serverClientId: "1052327499631-0panjl4ntatouhfvnf1b94880a4j4gpg.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true
+    }
+  }
 };
 
 export default config;
